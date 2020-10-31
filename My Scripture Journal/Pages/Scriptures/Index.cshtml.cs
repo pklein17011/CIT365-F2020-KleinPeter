41240536be0c33e3e0366f2ea88f41609ac00170
pklein17011 <59589510+pklein17011@users.noreply.github.com>
2020-10-31 12:37:01 -0500

    My Scripture Journal

---
+++ My Scripture Journal/Pages/Scriptures/Index.cshtml.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using My_Scripture_Journal.Models;
+
+namespace My_Scripture_Journal.Pages.Scriptures
+{
+    public class IndexModel : PageModel
+    {
+        private readonly My_Scripture_Journal.Models.My_Scripture_JournalContext _context;
+
+        public IndexModel(My_Scripture_Journal.Models.My_Scripture_JournalContext context)
+        {
+            _context = context;
+        }
+
+        public IList<Scripture> Scripture { get;set; }
+        [BindProperty(SupportsGet = true)]
+        public string SearchString { get; set; }
+        // Requires using Microsoft.AspNetCore.Mvc.Rendering;
+        public SelectList Books { get; set; }
+        [BindProperty(SupportsGet = true)]
+        public string ScriptureBook { get; set; }
+
+        // For sorting
+        public string BookSort { get; set; }
+        public string DateSort { get; set; }
+        public string CurrentFilter { get; set; }
+        public string CurrentSort { get; set; }
+
+        public async Task OnGetAsync(string sortOrder)
+        {
+            // Use LINQ to get list of books.
+            IQueryable<string> bookQuery = from m in _context.Scripture
+                                            orderby m.Book
+                                            select m.Book;
+
+            var scriptures = from m in _context.Scripture
+                         select m;
+
+            if (!string.IsNullOrEmpty(SearchString))
+            {
+                scriptures = scriptures.Where(s => s.Note.Contains(SearchString));
+            }
+
+            if (!string.IsNullOrEmpty(ScriptureBook))
+            {
+                scriptures = scriptures.Where(x => x.Book == ScriptureBook);
+            }
+            Books = new SelectList(await bookQuery.Distinct().ToListAsync());
+            
+
+
+
+            BookSort = String.IsNullOrEmpty(sortOrder) ? "book_desc" : "";
+            DateSort = sortOrder == "Date" ? "date_desc" : "Date";
+
+
+            switch (sortOrder)
+            {
+                case "book_desc":
+                    scriptures = scriptures.OrderByDescending(s => s.Book);
+                    break;
+                case "Date":
+                    scriptures = scriptures.OrderBy(s => s.DateAdded);
+                    break;
+                case "date_desc":
+                    scriptures = scriptures.OrderByDescending(s => s.DateAdded);
+                    break;
+                default:
+                    scriptures = scriptures.OrderBy(s => s.Book);
+                    break;
+            }
+
+            Scripture = await scriptures.ToListAsync();
+        }
+    }
+}
